4dc92e38c5929d39a217fc74b4ef8cfa3244a4ec
ksanchez1215 <60160749+ksanchez1215@users.noreply.github.com>
2020-05-14 12:55:29 -0500

    Adding functions file

---
+++ R/Functions.R
@@ -0,0 +1,31 @@
+pca <- function(x, retx = TRUE, center = TRUE, scale. = FALSE, tol = NULL, ...)
+  {
+    chkDots(...)
+    x <- as.matrix(x)
+    x <- scale(x, center = center, scale = scale.)
+    cen <- attr(x, "scaled:center")
+    sc <- attr(x, "scaled:scale")
+    if(any(sc == 0))
+      stop("cannot rescale a constant/zero column to unit variance")
+    s <- svd(x, nu = 0)
+    s$d <- s$d / sqrt(max(1, nrow(x) - 1))
+    if (!is.null(tol)) {
+      ## we get rank at least one even for a 0 matrix.
+      rank <- sum(s$d > (s$d[1L]*tol))
+      if (rank < ncol(x)) {
+        s$v <- s$v[, 1L:rank, drop = FALSE]
+        s$d <- s$d[1L:rank]
+      }
+    }
+    dimnames(s$v) <-
+      list(colnames(x), paste0("PC", seq_len(ncol(s$v))))
+    r <- list(sdev = s$d, rotation = s$v,
+              center = if(is.null(cen)) FALSE else cen,
+              scale = if(is.null(sc)) FALSE else sc)
+    if (retx) r$x <- x %*% s$v
+    class(r) <- "prcomp"
+    r
+  }
+
+pca_tab <- function(x) {
+  summary(pca(x))}
